65c96a3d6cb4bcb4c852c9424cf3b590082c3fe0
Andrea Rettaroli <andrea.rettaroli@studio.unibo.it>
2024-04-14 01:33:16 +0200

    fixed permission, mvp send transaction

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -82,11 +82,7 @@ export const onUserInput: OnUserInputHandler = async ({ id, event }) => {
     const messageInputValue = event.value['message'];
     const result = successorInputValue?.concat(messageInputValue ?? '');
 
-    // This resolves to the value of window.ethereum or null.
-    const maybeProvider: SnapsEthereumProvider = ethereum;
-
     const web3Provider = new ethers.providers.Web3Provider(ethereum);
-    //const blockId = web3Provider.blockNumber;
 
     if (!web3Provider) {
       await showStoreResult(id, result ?? '');
@@ -128,122 +124,5 @@ export const onUserInput: OnUserInputHandler = async ({ id, event }) => {
   ) {
     const inputValue = event.value['restore-key'];
     await showVefiryResult(id, inputValue ?? '');
-  }
-};
-
-export const onRpcRequest: OnRpcRequestHandler = async ({ request }) => {
-  switch (request.method) {
-    case 'getAddress': {
-      const privateKey = await getPrivateKey();
-      const wallet = new Wallet(privateKey);
-
-      return await wallet.getAddress();
-    }
-
-    case 'signMessage': {
-      const params = request.params as SignMessageParams;
-      const privateKey = await getPrivateKey();
-      const wallet = new Wallet(privateKey);
-      const result = await snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'confirmation',
-          content: panel([
-            heading('Signature request'),
-            text('Do you want to sign this message?'),
-            copyable(params.message),
-          ]),
-        },
-      });
-      if (!result) {
-        throw new UserRejectedRequestError();
-      }
-      return wallet.signMessage(params.message);
-    }
-
-    default:
-      throw new MethodNotFoundError({ method: request.method });
-  }
-};
-
-export const onSignature: OnSignatureHandler = async ({ signature }) => {
-  const { signatureMethod, from, data } = signature;
-  let domain;
-  if (
-    signatureMethod === 'eth_signTypedData_v3' ||
-    signatureMethod === 'eth_signTypedData_v4'
-  ) {
-    domain = data.domain;
-  }
-
-  // const getSignedTypedDataRows = (typeData: Record<string, any>[]) => {
-  //   const typeCount = typeData.reduce(
-  //     (acc: Record<string, number>, currVal: Record<string, string>) => {
-  //       if (acc[currVal.type]) {
-  //         acc[currVal.type] += 1;
-  //       } else {
-  //         acc[currVal.type] = 1;
-  //       }
-  //       return acc;
-  //     },
-  //     {},
-  //   );
-  //   return Object.entries(typeCount).map(([type, count]) =>
-  //     row(type, text(`${count}`)),
-  //   );
-  // };
-
-  switch (signatureMethod) {
-    case 'eth_sign':
-      return {
-        content: panel([
-          heading("'About 'eth_sign'"),
-          text(
-            "eth_sign is one of the oldest signing methods that MetaMask still supports. Back in the early days of MetaMask when it was originally designed, web3 was quite different from the present day. There were fewer standards for signatures, so eth_sign was developed with a fairly simple, open-ended structure.\nThe main thing to note about eth_sign is that it allows the website you're on to request that you sign an arbitrary hash. In this mathematical context, 'arbitrary' means unspecified; your signature could be applied by the requesting dapp to pretty much anything. eth_sign is therefore unsuitable to use with sources that you don't trust.\nAdditionally, the way eth_sign is designed means that the contents of the message you're signing are not human-readable. It's impossible to check up on what you're actually signing, making it particularly dangerous.",
-          ),
-        ]),
-        severity: SeverityLevel.Critical,
-      };
-
-    case 'personal_sign':
-      return {
-        content: panel([row('From:', text(from)), row('Data:', text(data))]),
-        severity: SeverityLevel.Critical,
-      };
-
-    // case 'eth_signTypedData':
-    //   // Show a count of the different types.
-    //   return {
-    //     content: panel([
-    //       heading('Message type count'),
-    //       ...getSignedTypedDataRows(data),
-    //     ]),
-    //     severity: SeverityLevel.Critical,
-    //   };
-
-    case 'eth_signTypedData_v3':
-      return {
-        content: panel([
-          heading('Danger!'),
-          text(
-            `${domain.verifyingContract} has been identified as a malicious verifying contract.`,
-          ),
-        ]),
-        severity: SeverityLevel.Critical,
-      };
-
-    case 'eth_signTypedData_v4':
-      return {
-        content: panel([
-          heading('Danger!'),
-          text(
-            `${domain.verifyingContract} has been identified as a malicious verifying contract.`,
-          ),
-        ]),
-        severity: SeverityLevel.Critical,
-      };
-
-    default:
-      return null;
   }
 };
